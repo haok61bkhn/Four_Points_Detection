697e4fff74b4936b2c4bf95fee870f99a35d9be1
HAOBK <haok61bkhn@gmail.com>
2020-07-05 17:25:04 +0700

    commit

--- detection/config.py
+++ detection/config.py
@@ -4,11 +4,17 @@ import os
 def get_config():
     conf=Edict()
     conf.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     conf.confidence_threshold=0.5
     conf.top_k=5000
     conf.nms_threshold=0.4
     conf.keep_top_k=750
     conf.trained_model="detection/weights/mobilenet0.25_new.pth"
     conf.network="mobile0.25" # or resnet50
-    conf.cpu=False
+    if(conf.device==torch.device("cpu")):
+        conf.cpu=True
+        print("use cpu!")
+    else:
+        conf.cpu=False
+        print("use gpu!")
     return conf
